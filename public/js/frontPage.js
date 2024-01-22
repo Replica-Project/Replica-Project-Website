f6df92d4d74fc296271f66c9d84a9435817b72e4
Replica-Project <157048772+Replica-Project@users.noreply.github.com>
2024-01-22 11:32:02 +0100

    adfasf

--- public/js/frontPage.js
+++ public/js/frontPage.js
@@ -5,13 +5,49 @@ function shoesClicked() {
 }
 
 const tshirtButton = document.getElementById('tshirtButton');
-tshirtButton.addEventListener('click', shoesClicked);
+tshirtButton.addEventListener('click', tshirtClicked);
 function tshirtClicked() {
     window.location.href = "../thisisapage.html";
 }
 
-const tshirtButton = document.getElementById('tshirtButton');
-tshirtButton.addEventListener('click', shoesClicked);
-function tshirtClicked() {
+const jacketButton = document.getElementById('jacketsButton');
+jacketButton.addEventListener('click', jacketsClicked);
+function jacketsClicked() {
+    window.location.href = "../thisisapage.html";
+}
+
+const bottomsButton = document.getElementById('bottomsButton');
+bottomsButton.addEventListener('click', bottomsClicked);
+function bottomsClicked() {
+    window.location.href = "../thisisapage.html";
+}
+
+const hoodieButton = document.getElementById('hoodiesButton');
+hoodieButton.addEventListener('click', hoodieClicked);
+function hoodieClicked() {
+    window.location.href = "../thisisapage.html";
+}
+
+const headwearButton = document.getElementById('headwearButton');
+headwearButton.addEventListener('click', headwearClicked);
+function headwearClicked() {
+    window.location.href = "../thisisapage.html";
+}
+
+const jewelryButton = document.getElementById('jewelryButton');
+jewelryButton.addEventListener('click', jewelryClicked);
+function jewelryClicked() {
+    window.location.href = "../thisisapage.html";
+}
+
+const watchesButton = document.getElementById('watchesButton');
+tshirtButton.addEventListener('click', watchesClicked);
+function watchesClicked() {
+    window.location.href = "../thisisapage.html";
+}
+
+const miscButton = document.getElementById('miscButton');
+miscButton.addEventListener('click', miscClicked);
+function miscClicked() {
     window.location.href = "../thisisapage.html";
 }
