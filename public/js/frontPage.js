97e36d936722dfdcee6b604c318a3c63184d64b3
Replica-Project <157048772+Replica-Project@users.noreply.github.com>
2024-01-24 14:58:42 +0100

    Product List start

--- public/js/frontPage.js
+++ public/js/frontPage.js
@@ -1,53 +1,53 @@
 const shoesButton = document.getElementById('shoesButton');
 shoesButton.addEventListener('click', shoesClicked);
 function shoesClicked() {
-    window.location.href = "../thisisapage.html";
+    window.location.href = "../productList.html?cat=Shoes";
 }
 
 const tshirtButton = document.getElementById('tshirtButton');
 tshirtButton.addEventListener('click', tshirtClicked);
 function tshirtClicked() {
-    window.location.href = "../thisisapage.html";
+    window.location.href = "../productList.html?cat=T-Shirts";
 }
 
 const jacketButton = document.getElementById('jacketsButton');
 jacketButton.addEventListener('click', jacketsClicked);
 function jacketsClicked() {
-    window.location.href = "../thisisapage.html";
+    window.location.href = "../productList.html?cat=Jackets";
 }
 
 const bottomsButton = document.getElementById('bottomsButton');
 bottomsButton.addEventListener('click', bottomsClicked);
 function bottomsClicked() {
-    window.location.href = "../thisisapage.html";
+    window.location.href = "../productList.html?cat=Bottoms";
 }
 
 const hoodieButton = document.getElementById('hoodiesButton');
 hoodieButton.addEventListener('click', hoodieClicked);
 function hoodieClicked() {
-    window.location.href = "../thisisapage.html";
+    window.location.href = "../productList.html?cat=Hoodies";
 }
 
 const headwearButton = document.getElementById('headwearButton');
 headwearButton.addEventListener('click', headwearClicked);
 function headwearClicked() {
-    window.location.href = "../thisisapage.html";
+    window.location.href = "../productList.html?cat=Headwear";
 }
 
 const jewelryButton = document.getElementById('jewelryButton');
 jewelryButton.addEventListener('click', jewelryClicked);
 function jewelryClicked() {
-    window.location.href = "../thisisapage.html";
+    window.location.href = "../productList.html?cat=Jewelry";
 }
 
 const watchesButton = document.getElementById('watchesButton');
-tshirtButton.addEventListener('click', watchesClicked);
+watchesButton.addEventListener('click', watchesClicked);
 function watchesClicked() {
-    window.location.href = "../thisisapage.html";
+    window.location.href = "../productList.html?cat=Watches";
 }
 
 const miscButton = document.getElementById('miscButton');
 miscButton.addEventListener('click', miscClicked);
 function miscClicked() {
-    window.location.href = "../thisisapage.html";
+    window.location.href = "../productList.html?cat=Miscellaneous";
 }
--- public/js/productsList.js
+++ public/js/productsList.js
@@ -1,2 +1,23 @@
+var url = new URL(window.location.href);
+let category = url.searchParams.get('cat');
+let textHeading = document.getElementById("catHead");
+textHeading.innerText = "All Results for " + category;
 
-const category = 0;
+function generateProductList() {
+    let list = document.getElementById('product-list');
+
+    const products = [
+        { name: 'Fake ahh tshirt', price: 4.69 }
+    ];
+
+    products.forEach(product => {
+        const productDiv = document.createElement('div');
+        productDiv.className = 'productDiv ';
+        // Use backticks (`) for template literals
+        productDiv.innerHTML = `<h3>${product.name}</h3><p>Price: $${product.price.toFixed(2)}</p>`;
+        // Append the productDiv to the list
+        list.appendChild(productDiv);
+    });
+}
+
+generateProductList();
